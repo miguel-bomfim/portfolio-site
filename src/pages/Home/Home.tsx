5514b86b6e5078a8d28828775017f5f737ded37d
Miguel Bomfim <miguelaubom@gmail.com>
2022-11-21 20:19:05 -0300

    Update home image height

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,21 +13,9 @@ const Home: FC<HomeProps> = ({ name, homeImage }) => {
         justifyContent: "center",
         position: "relative",
         marginTop: "60px",
-        height: "700px",
+        height: "850px",
       }}
     >
-      {/* <span
-        style={{
-          position: "absolute",
-          textTransform: "uppercase",
-          fontWeight: "bold",
-          fontSize: "150px",
-          bottom: "45%",
-          color: "#ffcf60",
-        }}
-      >
-        {name}
-      </span> */}
       <img
         style={{ width: "100%", height: "100%", objectFit: "cover" }}
         src={homeImage}
