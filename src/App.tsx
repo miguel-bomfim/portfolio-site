14e0fa3b079d036275e9780c11cff0cf72541e55
Miguel Bomfim <miguelaubom@gmail.com>
2022-11-30 15:05:52 -0300

    Improve site layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import ScrollToTop from "./helpers/ScrollToTop";
+import Footer from "./components/Footer";
 
 function App() {
   const { data: homeData } = useHome();
@@ -64,6 +65,7 @@ function App() {
             />
           </Routes>
         </main>
+        <Footer />
       </ThemeProvider>
     </BrowserRouter>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
-import Footer from "./components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +13,6 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <Footer />
     </QueryClientProvider>
   </React.StrictMode>
 );
