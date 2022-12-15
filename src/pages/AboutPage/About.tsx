41b1b056ebe63f03fc324dca5530e1abe941e01e
Miguel Bomfim <miguelaubom@gmail.com>
2022-12-15 12:18:12 -0300

    Improve layout

--- src/pages/AboutPage/About.tsx
+++ src/pages/AboutPage/About.tsx
@@ -2,6 +2,9 @@ import { FC } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import classNames from "classnames";
 import useMobile from "../../hooks/useMobile";
+import useDelay from "../../hooks/useDelay";
+import Loading from "../../components/Loading";
+
 import "./About.css";
 interface AboutProps {
   image:
@@ -37,53 +40,61 @@ const About: FC<AboutProps> = ({
   const doc = parser.parseFromString(description || "", "text/html");
   const aboutParagraph = doc.getElementsByTagName("p");
   var paragraphs = Array.from(aboutParagraph);
+  const delayToRender = useDelay("aboutPage");
 
   return (
     <article className="aboutContainer">
-      <div className="aboutHeader">
-        <h2 className="aboutHeaderTitle">
-          Sarah
-          <br />
-          Heloísa
-        </h2>
-        <LazyLoadImage
-          wrapperClassName={classNames({
-            aboutImageWrapper: !isMobile,
-            aboutImageWrapperMobile: isMobile,
-          })}
-          className="aboutImage"
-          alt="foto da saris"
-          src={image?.url}
-          effect="blur"
-        />
-      </div>
+      {delayToRender ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="aboutHeader">
+            <h2 className="aboutHeaderTitle">
+              Sarah
+              <br />
+              Heloísa
+            </h2>
+            <LazyLoadImage
+              wrapperClassName={classNames({
+                aboutImageWrapper: !isMobile,
+                aboutImageWrapperMobile: isMobile,
+              })}
+              className="aboutImage"
+              alt="foto da saris"
+              src={image?.url}
+              effect="blur"
+            />
+          </div>
 
-      <p className="p0">{paragraphs[0]?.innerHTML}</p>
-      <div className="paragraphOneDiv">
-        <p className="p1">{paragraphs[1]?.innerHTML}</p>
-        <img className="paragraphOneImg" src={imgText?.url} alt="" />
-      </div>
-      <p className="p2">{paragraphs[2]?.innerHTML}</p>
-      <p className="p3">{paragraphs[3]?.innerHTML}</p>
-      <p className="p4">{paragraphs[4]?.innerHTML}</p>
-      <p className="p5">{paragraphs[5]?.innerHTML}</p>
+          <p className="p0">{paragraphs[0]?.innerHTML}</p>
+          <div className="paragraphOneDiv">
+            <p className="p1">{paragraphs[1]?.innerHTML}</p>
+            <img className="paragraphOneImg" src={imgText?.url} alt="" />
+          </div>
+          <p className="p2">{paragraphs[2]?.innerHTML}</p>
+          <p className="p3">{paragraphs[3]?.innerHTML}</p>
+          <p className="p4">{paragraphs[4]?.innerHTML}</p>
+          <p className="p5">{paragraphs[5]?.innerHTML}</p>
 
-      <div className="endPresentationContainer">
-        <h2 className="endPresentationTitle1">Vamos</h2>
-        <div className="endPresentation">
-          {portfolioExamples?.map((portfolio, idx) => {
-            return (
-              <img
-                key={idx}
-                style={{ maxWidth: "20%" }}
-                src={portfolio.url}
-                alt=""
-              />
-            );
-          })}
-        </div>
-        <h2 className="endPresentationTitle2"> criar</h2>
-      </div>
+          <div className="endPresentationContainer">
+            <h2 className="endPresentationTitle1">Vamos</h2>
+            <div className="endPresentation">
+              {portfolioExamples?.map((portfolio, idx) => {
+                return (
+                  <img
+                    key={idx}
+                    className={`endImage${idx}`}
+                    style={{ maxWidth: "20%" }}
+                    src={portfolio.url}
+                    alt=""
+                  />
+                );
+              })}
+            </div>
+            <h2 className="endPresentationTitle2"> criar</h2>
+          </div>
+        </>
+      )}
     </article>
   );
 };
--- src/pages/Portifolio/PortfolioPhotos.tsx
+++ src/pages/Portifolio/PortfolioPhotos.tsx
@@ -4,10 +4,12 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import useMobile from "../../hooks/useMobile";
+import useDelay from "../../hooks/useDelay";
+
 import "./Portfolio.css";
 import DialogSlider from "../../components/DialogSlider";
 import { useEssay } from "../../services";
-
+import Loading from "../../components/Loading";
 interface EssayProps {
   url: string;
   id: string;
@@ -18,15 +20,18 @@ interface EssayProps {
 const PortfolioPhotos = () => {
   const { pathname } = useLocation();
   const slug = pathname.substring(11);
-  const { data: essay, isLoading } = useEssay(slug);
+  const { data: essay, isLoading: loadingEssay } = useEssay(slug);
   const [showSlider, setShowSlider] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const delayToRender = useDelay("porfolioPage");
 
   const isMobile = useMobile();
 
   return (
     <div>
-      {!isLoading && (
+      {delayToRender ? (
+        <Loading />
+      ) : (
         <div className="containerPhotos">
           <ImageList
             sx={{
@@ -36,38 +41,40 @@ const PortfolioPhotos = () => {
             }}
             cols={1}
           >
-            {essay.map((item: EssayProps, idx: number) => {
-              if (item.height > item.width) {
-                item.height = 1117;
-                item.width = 740;
-              } else {
-                item.height = 493;
-                item.width = 740;
-              }
+            {!loadingEssay &&
+              essay?.map((item: EssayProps, idx: number) => {
+                if (item.height > item.width) {
+                  item.height = 1117;
+                  item.width = 740;
+                } else {
+                  item.height = 493;
+                  item.width = 740;
+                }
 
-              return (
-                <ImageListItem sx={{ overflowY: "hidden" }} key={idx}>
-                  <div
-                    className="image"
-                    onClick={() => {
-                      setCurrentSlide(idx);
-                      !isMobile && setShowSlider(!showSlider);
-                    }}
-                    key={idx}
-                  >
-                    <LazyLoadImage
-                      src={item.url}
+                return (
+                  <ImageListItem sx={{ overflowY: "hidden" }} key={idx}>
+                    <div
+                      className="image"
+                      onClick={() => {
+                        setCurrentSlide(idx);
+                        !isMobile && setShowSlider(!showSlider);
+                      }}
                       key={idx}
-                      alt=""
-                      height={isMobile ? "100%" : item.height}
-                      width={isMobile ? "100%" : item.width}
-                      effect="blur"
-                      placeholderSrc={item.url}
-                    />
-                  </div>
-                </ImageListItem>
-              );
-            })}
+                    >
+                      <LazyLoadImage
+                        src={item.url}
+                        srcSet={item.url}
+                        key={idx}
+                        alt=""
+                        height={isMobile ? "100%" : item.height}
+                        width={isMobile ? "100%" : item.width}
+                        effect="blur"
+                        placeholderSrc={item.url}
+                      />
+                    </div>
+                  </ImageListItem>
+                );
+              })}
           </ImageList>
         </div>
       )}
